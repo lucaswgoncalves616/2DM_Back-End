5e2c1db4fc0c4097ca67ce3904248469116fb10c
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2025-08-26 11:18:59 -0300

    Slot Machine

--- src/SlotMachine/Main.java
+++ src/SlotMachine/Main.java
@@ -1,14 +1,31 @@
 package SlotMachine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String lion = "🦁";
+        String tiger = "🐯";
+        String fox = "🦊";
+        String bear = "🐻";
+        String horse = "🐴";
+
+        int balance = 100;
+
         System.out.println("""
                 **********************
-                Welcome to Java Slots
-                Symbols: 🦁🐯🦊🐻🐴
+                Bem vindo ao Java Slot
+                Simbolos: 🦁🐯🦊🐻🐴
                 **********************
                 """);
-    }
 
-    String cavalo = "🐴";
+        System.out.println("Saldo atual: " + balance);
+
+        System.out.println("Digite o quanto quer apostar: ");
+        balance = sc.nextInt();
+
+
+    }
 }
