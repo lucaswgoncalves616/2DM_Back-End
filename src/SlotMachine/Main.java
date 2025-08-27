a791f1ea8cb9847cd18b25f86c351256c16d0812
Lucas Wendell <lucas.wendell.98@gmail.com>
2025-08-27 12:45:48 -0300

    Slot Machine

--- src/SlotMachine/Main.java
+++ src/SlotMachine/Main.java
@@ -3,16 +3,15 @@ package SlotMachine;
 import java.util.Scanner;
 
 public class Main {
+    public static String formatReal(double balance) {
+        return "R$ " + String.format("%.2f", balance);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String lion = "🦁";
-        String tiger = "🐯";
-        String fox = "🦊";
-        String bear = "🐻";
-        String horse = "🐴";
-
-        int balance = 100;
+        String[] row = {"🦁","🐯","🦊","🐻","🐴"};
+        double balance = 100;
 
         System.out.println("""
                 **********************
@@ -21,11 +20,24 @@ public class Main {
                 **********************
                 """);
 
-        System.out.println("Saldo atual: " + balance);
+        System.out.println("Saldo atual: " + formatReal(balance));
 
         System.out.println("Digite o quanto quer apostar: ");
-        balance = sc.nextInt();
+        balance -= sc.nextDouble();
 
+        System.out.println("Saldo atual: " + formatReal(balance));
 
+        String mensagem = "Olá, isso é uma animação no console!";
+
+        for (char c : mensagem.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(100); // atraso de 100ms entre cada caractere
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(); // pula linha no final
     }
 }
