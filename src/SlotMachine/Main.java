dc5285df442d2953d586b67880474a2f17ca393f
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2025-08-28 11:08:19 -0300

    Slot Machine

--- src/SlotMachine/Main.java
+++ src/SlotMachine/Main.java
@@ -7,11 +7,29 @@ public class Main {
         return "R$ " + String.format("%.2f", balance);
     }
 
+    public static double makeBet(double balance){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o quanto quer apostar: ");
+        double bet = sc.nextDouble();
+
+        while(true){
+            if (bet > balance) {
+                System.out.println("Valor da aposta deve ser menor que o saldo!");
+                bet = sc.nextDouble();
+            } else if (bet < 0) {
+                System.out.println("Valor da aposta não pode ser negativo!");
+                bet = sc.nextDouble();
+            } else {
+                break;
+            }
+        }
+        return balance - bet;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        String[] row = {"🦁","🐯","🦊","🐻","🐴"};
+        String[] row = {"🦁","🐯","🦊","🐻","🐴","🦄"};
         double balance = 100;
 
         System.out.println("""
@@ -23,11 +41,10 @@ public class Main {
 
         System.out.println("Saldo atual: " + formatReal(balance));
 
-        System.out.println("Digite o quanto quer apostar: ");
-        balance -= sc.nextDouble();
+        balance = makeBet(balance);
 
         System.out.println("Saldo atual: " + formatReal(balance));
-
+/*
         String mensagem = "Olá, isso é uma animação no console!";
 
         for (char c : mensagem.toCharArray()) {
@@ -39,6 +56,6 @@ public class Main {
             }
         }
 
-        System.out.println(); // pula linha no final
+        System.out.println(); // pula linha no final*/
     }
 }
